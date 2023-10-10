431156f59ff7f90adc08c041326fc02fae2de8b2
NguyenHuuHung <hhnguyenit@gmail.com>
2023-10-11 00:55:20 +0700

    feat: add inline interatctions

--- Password/Password/ViewController.swift
+++ Password/Password/ViewController.swift
@@ -32,6 +32,7 @@ extension ViewController {
         stackView.spacing = 20
         
         newPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
+        newPasswordTextField.delegate = self
         
         statusView.translatesAutoresizingMaskIntoConstraints = false
         statusView.layer.cornerRadius = 5
@@ -59,4 +60,15 @@ extension ViewController {
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2),
         ])
     }
+}
+
+// MARK: PasswordTextFieldDelegate
+extension ViewController: PasswordTextFieldDelegate {
+    func editingChanged(_ sender: PasswordTextField) {
+        if sender === newPasswordTextField {
+             statusView.updateDisplay(sender.textField.text ?? "")
+        }
+    }
+    
+    
 }
